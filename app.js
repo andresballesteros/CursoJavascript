a157f1d1fa6f5fa9e6068227b2d90f249dccc02e
andres.pc <andres_llopo@hotmail.com>
2020-03-14 07:50:59 -0500

    Inicializacion de curso de java en git

---
+++ app.js
@@ -0,0 +1,21 @@
+let numberOne = 600;
+let numberTwo = 100;
+let result = numberOne + numberTwo;
+let names = ["Andres", "Goku", "Vegeta", "Gohan"];
+/*console.log(result);
+for(let i = 0; i<names.length; i++){
+    console.log("Hola "+names[i]);
+}*/
+console.log(names);
+names.forEach(element => {
+  console.log("Hola " + element);
+});
+
+function greeting(name) {
+  console.log("Hola como estas " + name);
+}
+
+console.log(names);
+names.forEach(element => {
+  greeting(element);
+});
