dc7e80ed92e790f329d21baddc9f9c72c2915d7c
andres.pc <andres_llopo@hotmail.com>
2020-03-17 18:48:09 -0500

    DOM con javascript

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let names = ["Andres", "Goku", "Vegeta", "Gohan"];
 /*console.log(result);
 for(let i = 0; i<names.length; i++){
     console.log("Hola "+names[i]);
-}*/
+}
 console.log(names);
 names.forEach(element => {
   console.log("Hola " + element);
@@ -18,8 +18,24 @@ function greeting(name) {
 console.log(names);
 names.forEach(element => {
   greeting(element);
-});
+});*/
 
-function holamundo(){
-  alert('Hola mundo desde función');
+function holamundo() {
+  alert("Hola mundo desde función");
+}
+
+function ampliarInfo() {
+  let resumen = document.getElementById("textoResumen");
+  let ampliado = document.getElementById("textoAmpliado");
+  let enlace = document.getElementById("enlace");
+
+  if (resumen.style.display != "none") {
+    ampliado.style.display = "block";
+    resumen.style.display = "none";
+    enlace.innerHTML = "Ocultar Info";
+  } else {
+    ampliado.style.display = "none";
+    resumen.style.display = "block";
+    enlace.innerHTML = "Ampliar info";
+  }
 }
